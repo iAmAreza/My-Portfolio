404593afc2a3d5a3ab7ce5d5aa20a68c1813d19e
IAmAreza <ahmadrezacuet.cse@gmail.com>
2025-01-07 19:47:34 +0600

    portfolio page added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar/Navbar'
 import Home from './components/Home/Home'
 import SocialLinks from './components/SocialLinks/SocialLinks'
 import About from './components/About/About'
+import Portfolio from './components/Portfolio/Portfolio'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Home />
       <About/>
+      <Portfolio/>
       <SocialLinks />
     </>
   )
---
+++ src/components/Portfolio/Portfolio.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import project1 from '../../assets/images/project 1.png';
+import project2 from '../../assets/images/project 2.png';
+import project3 from '../../assets/images/project 3.png';
+
+function Portfolio() {
+  const porfolios = [
+    {
+      id: 1,
+      src: project1,
+      link: 'https://github.com/iAmAreza/Diabetes_Prediction_Flask_Project',
+    },
+    {
+      id: 2,
+      src: project2,
+      link: 'https://github.com/iAmAreza/Django-Full-stack-CRUD',
+    },
+    {
+      id: 3,
+      src: project3,
+      link: 'https://github.com/iAmAreza/Fetch-Youtube-Comments',
+    },
+  ];
+
+  return (
+    <div name="portfolio" className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">Portfolio</p>
+          <p className="py-6">Check out some of my work right here</p>
+        </div>
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-14 sm:px-0">
+          {porfolios.map(({ id, src, link }) => (
+            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+              <img src={src} alt="" className="rounded-md duration-200 hover:scale-105" />
+              <div className="flex items-center justify-center">
+                <button
+                  onClick={() => window.open(link, '_blank')} // Opens link in a new tab
+                  className="w-full px-6 py-3 m-4 duration-200 hover:scale-105"
+                >
+                  Source Code
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Portfolio;
